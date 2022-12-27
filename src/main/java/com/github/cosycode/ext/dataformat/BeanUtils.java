02cc59dd8b8b8f5fa1c30040524ff74062d5cf72
sinjar.chen <sinjar.chen@qq.com>
2022-12-28 02:09:43 +0800

    修复工具类

---
+++ src/main/java/com/github/cosycode/ext/dataformat/BeanUtils.java
@@ -0,0 +1,60 @@
+package com.github.cosycode.ext.dataformat;
+
+import java.lang.reflect.Field;
+
+/**
+ * <b>Description : </b>
+ * <p>
+ * <b>created in </b> 2022/10/17
+ * </p>
+ *
+ * @author pengfchen
+ * @since
+ **/
+public class BeanUtils {
+
+    public static String[] getFieldString(Class<?> clazz) {
+        Field[] declaredFields = clazz.getDeclaredFields();
+        String[] filedString = new String[declaredFields.length];
+        for (int i = 0, len = declaredFields.length; i < len; i++) {
+            filedString[i] = declaredFields[i].getName();
+        }
+        return filedString;
+    }
+
+    public static <T> Object[] getValueObj(Class<? super T> clazz, T t) {
+        Field[] declaredFields = clazz.getDeclaredFields();
+        Object[] fieldObj = new Object[declaredFields.length];
+        for (int i = 0, len = declaredFields.length; i < len; i++) {
+            Field field = declaredFields[i];
+            field.setAccessible(true);
+            try {
+                fieldObj[i] = field.get(t);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return fieldObj;
+    }
+
+    public static <T> String[] getStringValueObj(Class<? super T> clazz, T t) {
+        Field[] declaredFields = clazz.getDeclaredFields();
+        String[] stringArr = new String[declaredFields.length];
+        for (int i = 0, len = declaredFields.length; i < len; i++) {
+            Field field = declaredFields[i];
+            field.setAccessible(true);
+            try {
+                Object obj = field.get(t);
+                if (obj == null) {
+                    stringArr[i] = "";
+                } else {
+                    stringArr[i] = TypeConverter.convertObjToString(obj);
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return stringArr;
+    }
+
+}
---
+++ src/main/java/com/github/cosycode/ext/dataformat/CsvUtils.java
@@ -0,0 +1,158 @@
+package com.github.cosycode.ext.dataformat;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
+import lombok.NonNull;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * <b>Description : </b>
+ * <p>
+ * <b>created in </b> 2022/9/6
+ * </p>
+ *
+ * @author pengfchen
+ * @since 0.2.2
+ **/
+public class CsvUtils {
+
+    private CsvUtils() {
+    }
+
+    /**
+     * 读取csv，返回一个Iterator<String[]>，每个String[]表示csv中的一行数据
+     *
+     * @param csvFile csv 文件 对象
+     * @return List<String [ ]> 读取的数据
+     * @throws IOException
+     * @throws CsvException
+     */
+    public static List<String[]> readCSV(File csvFile) throws IOException, CsvException {
+        DataInputStream dataInputStream = new DataInputStream(Files.newInputStream(csvFile.toPath()));
+        CSVReader csvReader = new CSVReader(new InputStreamReader(dataInputStream, StandardCharsets.UTF_8));
+        return csvReader.readAll();
+    }
+
+
+    /**
+     * @param csvFile csv 文件
+     * @param tClass  模版类
+     * @param <T>     模版类
+     * @return 读取的 list
+     * @throws IOException
+     * @throws CsvException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public static <T> List<T> readCsvToBeanList(File csvFile, Class<T> tClass) throws IOException, CsvException, InstantiationException, IllegalAccessException, ParseException {
+        List<String[]> strings = readCSV(csvFile);
+        if (strings == null) {
+            return new ArrayList<>();
+        }
+
+        // 封装函数
+        Field[] fields = tClass.getDeclaredFields();
+        Function<String, Field> function = name -> {
+            for (Field field : fields) {
+                if (field.getName().equalsIgnoreCase(name)) {
+                    return field;
+                }
+            }
+            return null;
+        };
+        // 获取header
+        List<Field> list = new ArrayList<>();
+        String[] headerString = strings.get(0);
+        for (String str : headerString) {
+            str = str.trim();
+            Field apply = function.apply(str);
+            if (apply != null) {
+                apply.setAccessible(true);
+                list.add(apply);
+            }
+        }
+        Field[] header = list.toArray(new Field[0]);
+        // 获取 body
+        List<T> result = new ArrayList<>();
+        int len = header.length;
+        int rowNum = 1;
+        for (int rowLen = strings.size(); rowNum < rowLen; rowNum++) {
+            String[] data = strings.get(rowNum);
+            T t = tClass.newInstance();
+            for (int i = 0; i < len; i++) {
+                Field field = header[i];
+                if (field == null) {
+                    continue;
+                }
+                String da = data[i] == null ? "" : data[i].trim();
+                Class<?> type = field.getType();
+                Object obj = TypeConverter.convertStringToObj(da, type);
+                field.set(t, obj);
+            }
+            result.add(t);
+        }
+        return result;
+    }
+
+
+    public static <T> void writeBeanListToCsvFile(File file, @NonNull List<T> list, Class<? super T> tClass) throws IOException {
+        if (list.isEmpty()) {
+            return;
+        }
+        Writer writer = new FileWriter(file);
+        try (CSVWriter csvWriter = new CSVWriter(writer)) {
+            String[] fieldString = BeanUtils.getFieldString(tClass);
+            csvWriter.writeNext(fieldString);
+            for (T obj : list) {
+                String[] strings = BeanUtils.getStringValueObj(tClass, obj);
+                csvWriter.writeNext(strings);
+            }
+        }
+    }
+
+
+    /**
+     * <p>
+     * 将 csv 中的行首 和 一行数据转换成一个 Map
+     * </p>
+     *
+     * @param keys   json的key，顺序需与csv中的value对应
+     * @param values csv中数据作为value
+     */
+    public static Map<String, Object> convertMap(String[] keys, String[] values) {
+        Map<String, Object> json = new HashMap<>();
+        for (int i = 0; i < keys.length; i++) {
+            json.put(keys[i], values[i]);
+        }
+        return json;
+    }
+
+    /**
+     * <p>
+     * 将 csv 中的行首 和 多行数据转换成一个 MapList
+     * </p>
+     *
+     * @param keys        行首，顺序需与csv中的value对应
+     * @param stringsList 读取csv返回的List<String[]>
+     */
+    public static List<Map<String, Object>> convertMap(String[] keys, List<String[]> stringsList) {
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        for (String[] strings : stringsList) {
+            Map<String, Object> json = convertMap(keys, strings);
+            mapList.add(json);
+        }
+        return mapList;
+    }
+
+}
